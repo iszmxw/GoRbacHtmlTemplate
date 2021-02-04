329e6f5ffb264fdaad475a9f5488c551cdde1cc4
追梦小窝 <mail@54zm.com>
2021-02-04 23:04:58 +0800

    登录日志操作日志页面调整

--- src/views/system/loginLog.vue
+++ src/views/system/loginLog.vue
@@ -1,72 +1,68 @@
 <template>
-  <div class="app-container">
-    <div class="filter-container">
-      <div class="el-card__header">
-        <div class="clearfix">
-          <router-link to="/dashboard">
-            <el-button type="primary">登录日志</el-button>
-          </router-link>
-          <router-link to="/system/operationLog">
-            <el-button>操作日志</el-button>
-          </router-link>
-        </div>
-      </div>
-    </div>
-
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%;"
-    >
-      <el-table-column
-        label="序号"
-        prop="id"
-        sortable="custom"
-        align="center"
-        width="80"
+  <div class="main">
+    <el-card class="box-card">
+      <div
+        slot="header"
+        class="clearfix"
       >
-        <template slot-scope="scope">
-          <span>{{ scope.row.id }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="用户名" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.username }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="角色" align="center">
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.account_id === 1" type="info">{{ scope.row.role_name }}</el-tag>
-          <el-tag v-else type="success">{{ scope.row.role_name }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column label="登录IP">
-        <template slot-scope="scope">
-          <span class="link-type">{{ scope.row.ip }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="登录地址" align="center">
-        <template slot-scope="{row}">
-          <el-tag>{{ row.address }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column label="时间" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.created_at | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+        <span>登录日志</span>
+      </div>
 
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="listQuery.page"
-      :limit.sync="listQuery.limit"
-      @pagination="getList"
-    />
+      <el-table
+        v-loading="listLoading"
+        :data="list"
+        border
+        fit
+        highlight-current-row
+        style="width: 100%;"
+      >
+        <el-table-column
+          label="序号"
+          prop="id"
+          sortable="custom"
+          align="center"
+          width="80"
+        >
+          <template slot-scope="scope">
+            <span>{{ scope.row.id }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="用户名" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.username }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="角色" align="center">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.account_id === 1" type="info">{{ scope.row.role_name }}</el-tag>
+            <el-tag v-else type="success">{{ scope.row.role_name }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="登录IP">
+          <template slot-scope="scope">
+            <span class="link-type">{{ scope.row.ip }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="登录地址" align="center">
+          <template slot-scope="{row}">
+            <el-tag>{{ row.address }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="时间" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.created_at | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <pagination
+        v-show="total>0"
+        :total="total"
+        :page.sync="listQuery.page"
+        :limit.sync="listQuery.limit"
+        @pagination="getList"
+      />
+    </el-card>
   </div>
 </template>
 
@@ -108,3 +104,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.main {
+  padding: 32px;
+}
+</style>
--- src/views/system/operationLog.vue
+++ src/views/system/operationLog.vue
@@ -1,67 +1,59 @@
 <template>
-  <div class="app-container">
-    <div class="filter-container">
-      <div class="el-card__header">
-        <div class="clearfix">
-          <router-link to="/dashboard">
-            <el-button>登录日志</el-button>
-          </router-link>
-          <router-link to="/system/operationLog">
-            <el-button type="primary">操作日志</el-button>
-          </router-link>
-        </div>
-      </div>
-    </div>
-
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%;"
-    >
-      <el-table-column
-        label="ID"
-        prop="id"
-        sortable="custom"
-        align="center"
-        width="80"
+  <div class="main">
+    <el-card class="box-card">
+      <div
+        slot="header"
+        class="clearfix"
       >
-        <template slot-scope="scope" width="80px">
-          <span>{{ scope.row.id }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="用户名" align="center" width="120px">
-        <template slot-scope="scope">
-          <span>{{ scope.row.username }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="角色" align="center" width="140px">
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.account_id === 1" type="info">{{ scope.row.role_name }}</el-tag>
-          <el-tag v-else type="success">{{ scope.row.role_name }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作内容" align="center">
-        <template slot-scope="{row}">
-          <el-tag>{{ row.content }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column label="时间" align="center" width="140px">
-        <template slot-scope="scope">
-          <span>{{ scope.row.created_at | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+        <span>操作日志</span>
+      </div>
 
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="listQuery.page"
-      :limit.sync="listQuery.limit"
-      @pagination="getList"
-    />
+      <el-table
+        v-loading="listLoading"
+        :data="list"
+      >
+        <el-table-column
+          label="ID"
+          prop="id"
+          sortable="custom"
+          align="center"
+          width="80"
+        >
+          <template slot-scope="scope" width="80px">
+            <span>{{ scope.row.id }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="用户名" align="center" width="120px">
+          <template slot-scope="scope">
+            <span>{{ scope.row.username }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="角色" align="center" width="140px">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.account_id === 1" type="info">{{ scope.row.role_name }}</el-tag>
+            <el-tag v-else type="success">{{ scope.row.role_name }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作内容" align="center">
+          <template slot-scope="{row}">
+            <el-tag>{{ row.content }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="时间" align="center" width="140px">
+          <template slot-scope="scope">
+            <span>{{ scope.row.created_at | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <pagination
+        v-show="total>0"
+        :total="total"
+        :page.sync="listQuery.page"
+        :limit.sync="listQuery.limit"
+        @pagination="getList"
+      />
+    </el-card>
   </div>
 </template>
 
@@ -71,7 +63,6 @@ import waves from '@/directive/waves' // waves directive
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 
 export default {
-  name: 'ComplexTable',
   components: { Pagination },
   directives: { waves },
   data() {
@@ -104,3 +95,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.main {
+  padding: 32px;
+}
+</style>
